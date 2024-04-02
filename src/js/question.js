c20dca06748878d3fcc476b7665dd22ad6a24225
CoC12 <kouhei121613@gmail.com>
2024-04-02 23:21:41 +0900

    optimize test case step process

--- src/js/question.js
+++ src/js/question.js
@@ -52,7 +52,7 @@ class TestCase {
      * @param {string} id テストケースID
      * @param {string} name テストケース名
      * @param {string} description テストケース説明
-     * @param {Array<object>} steps テストケース処理のリスト
+     * @param {Array<Step>} steps テストケース処理のリスト
      */
     constructor(
         id,
@@ -76,7 +76,7 @@ class TestCase {
             json.id,
             json.name,
             json.description,
-            json.steps,
+            json.steps.map(step => stepFactory(step)),
         );
     }
 
@@ -89,32 +89,7 @@ class TestCase {
     isAccepted(targetDocument, debug = false) {
         let result = true;
         try {
-            this.steps.forEach((step) => {
-                const repeatCount = step.repeat || 1;
-                for (let i = 0; i < repeatCount; i++) {
-                    const element = targetDocument.querySelector(step.selector);
-                    switch (step.type) {
-                        case 'input':
-                            element.value = step.value;
-                            break;
-                        case 'click':
-                            element.click();
-                            break;
-                        case 'assert':
-                            if (element.textContent !== step.value) {
-                                throw new Error(`Assertion failed: ${element.textContent} !== ${step.value}`);
-                            }
-                            break;
-                        case 'assertAttribute':
-                            if (element[step.attribute] !== step.value) {
-                                throw new Error(`Assertion failed: ${element[step.attribute]} !== ${step.value}`);
-                            }
-                            break;
-                        default:
-                            throw new Error(`Unknown step type: ${step.type}`);
-                    }
-                }
-            });
+            this.steps.forEach((step) => step.execute(targetDocument));
         } catch (e) {
             if (debug) {
                 console.error(`ID: ${this.id}, name: ${this.name}\n`, e);
@@ -124,3 +99,118 @@ class TestCase {
         return result;
     }
 }
+
+
+class BaseStep {
+    /**
+     * コンストラクタ
+     * @param {string} type 種別
+     * @param {string} selector セレクタ
+     * @param {number} repeat 繰り返し回数
+     * @param {object} options オプション値
+     */
+    constructor(
+        type,
+        selector,
+        repeat,
+        options,
+    ) {
+        this.type = type;
+        this.selector = selector;
+        this.repeat = repeat;
+        this.options = options;
+    }
+
+    /**
+     * ステップを実行する。
+     * @param {Document} targetDocument テスト対象のDocument
+     */
+    execute(targetDocument) {
+        for (let i = 0; i < this.repeat; i++) {
+            const element = targetDocument.querySelector(this.selector);
+            this._execute(element);
+        }
+    }
+
+    /**
+     * ステップを実行する。
+     * （中身は、子クラスで実装）
+     * @param {Element} targetElement 処理対象の要素
+     */
+    _execute(targetElement) {
+        throw new Error('Not implemented');
+    }
+}
+
+
+class ClickStep extends BaseStep {
+    /**
+     * ステップを実行する。
+     * @param {Element} targetElement 処理対象の要素
+     */
+    _execute(targetElement) {
+        targetElement.click();
+    }
+}
+
+
+class InputStep extends BaseStep {
+    /**
+     * ステップを実行する。
+     * @param {Element} targetElement 処理対象の要素
+     */
+    _execute(targetElement) {
+        targetElement.value = this.options.value;
+    }
+}
+
+
+class AssertStep extends BaseStep {
+    /**
+     * ステップを実行する。
+     * @param {Element} targetElement 処理対象の要素
+     */
+    _execute(targetElement) {
+        const expectedValue = this.options.value;
+        if (targetElement.textContent !== expectedValue) {
+            throw new Error(`Assertion failed: ${targetElement.textContent} !== ${expectedValue}`);
+        }
+    }
+}
+
+
+class AssertAttributeStep extends BaseStep {
+    /**
+     * ステップを実行する。
+     * @param {Element} targetElement 処理対象の要素
+     */
+    _execute(targetElement) {
+        const attribute = this.options.attribute;
+        const expectedValue = this.options.value;
+        if (targetElement[attribute] !== expectedValue) {
+            throw new Error(`Assertion failed: ${targetElement[attribute]} !== ${expectedValue}`);
+        }
+    }
+}
+
+
+const stepTypes = {
+    input: InputStep,
+    click: ClickStep,
+    assert: AssertStep,
+    assertAttribute: AssertAttributeStep,
+};
+
+
+/**
+ * JSON オブジェクトから Step オブジェクトを生成する。
+ * @param {JSON} json JSON オブジェクト
+ * @return {BaseStep} Step オブジェクト
+ */
+const stepFactory = (json) => {
+    const type = json.type;
+    const selector = json.selector;
+    const repeat = json.repeat || 1;
+    const options = json;
+    return new stepTypes[type](type, selector, repeat, options);
+};
